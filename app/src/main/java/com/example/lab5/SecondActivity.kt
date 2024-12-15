139f8cf1408661c289388df8ff321d5f58dc1a1b
tommykorch <tkorchmit@bk.ru>
2024-12-16 02:13:19 +0700

    Part4

--- app/src/main/java/com/example/lab5/SecondActivity.kt
+++ app/src/main/java/com/example/lab5/SecondActivity.kt
@@ -1,20 +1,30 @@
 package com.example.lab5
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
+
+        val Selected_Radio = intent.getStringExtra("Selected_Radio")
+        val timeValue = intent.getStringExtra("Time")?.toIntOrNull() ?: 0
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        
     }
 }
