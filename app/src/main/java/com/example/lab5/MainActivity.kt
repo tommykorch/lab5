f4cbf59b33b55971034ec05080107d96628d6b0f
tommykorch <tkorchmit@bk.ru>
2024-12-16 02:07:03 +0700

    Part2

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -12,5 +12,39 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btn: Button
+    private lateinit var time: EditText
+    private lateinit var radioButton1: RadioButton
+    private lateinit var radioButton2: RadioButton
+    private lateinit var radioButton3: RadioButton
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        radioButton1=findViewById(R.id.radioButton)
+        radioButton2=findViewById(R.id.radioButton2)
+        radioButton3=findViewById(R.id.radioButton3)
+        time=findViewById(R.id.editText)
+        btn=findViewById(R.id.button)
+
+        btn.setOnClickListener()
+        {
+            val intent=Intent(this,SecondActivity::class.java)
+            when{
+                radioButton1.isChecked->intent.putExtra("Selected_Radio","RadioButton1")
+                radioButton2.isChecked->intent.putExtra("Selected_Radio","RadioButton2")
+                radioButton3.isChecked->intent.putExtra("Selected_Radio","RadioButton3")
+            }
+            intent.putExtra("Time",time.text.toString())
+            startActivity(intent)
+        }
+
+    }
 }
+
