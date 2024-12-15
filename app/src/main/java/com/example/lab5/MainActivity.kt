f7ca654a6a458b9d3578926c5f034d62cfc36236
tommykorch <tkorchmit@bk.ru>
2024-12-16 02:00:53 +0700

    Start Project

---
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lab5
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+}
